9569ce564eba788ece263ada5e21e823f13f507b
mjmacam20 <arde.macam.up@phinmaed.com>
2024-09-07 17:18:22 +0800

    commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:web3modal_flutter/web3modal_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late W3MService _w3mService;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeState();
+  }
+
+  void initializeState() async {
+    _w3mService = W3MService(
+      projectId: '2d5e262acbc9bf4a4ee3102881528534',
+      metadata: const PairingMetadata(
+        name: 'Web3Modal Flutter Example',
+        description: 'Web3Modal Flutter Example',
+        url: 'https://www.walletconnect.com/',
+        icons: ['https://walletconnect.com/walletconnect-logo.png'],
+        redirect: Redirect(
+          native: 'w3m://',
+          universal: 'https://www.walletconnect.com',
+        ),
+      ),
+    );
+    await _w3mService.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        W3MConnectWalletButton(service: _w3mService),
+        const SizedBox(height: 16),
+        W3MNetworkSelectButton(service: _w3mService),
+        const SizedBox(height: 16),
+        W3MAccountButton(service: _w3mService),
+      ],
+    );
+  }
+}
